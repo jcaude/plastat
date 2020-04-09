a761e4c45135e43aa145cc55dde9ee17458bebda
Jean-Christophe AUDE <jc.aude@gmail.com>
2020-04-09 15:47:07 +0200

    [UPD] SC import

--- R/import.R
+++ R/import.R
@@ -1,1 +1,13 @@
-print("Hello world...")
+library(readxl)
+
+
+readSC <- function(file = "Data/Shotgun_Plastic.xlsx") {
+  dataset.sc <- read_xlsx(path = file,
+                          sheet = "compar spectra",
+                          col_names = TRUE)
+  colnames(dataset.sc) <- c("ProteinID", "P3","P2","P1","C3","C2","C1")
+  return(dataset.sc)
+}
+
+
+
